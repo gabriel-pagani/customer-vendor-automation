86937e12b8b6171ce3f398de5abd9f9f19b64b0d
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-01-21 19:49:54 -0300

    Update cnpj_lookup call to use keyword arguments for clarity

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from apis.customer_vendor import create_new_customer_vendor
 from apis.receitaws import cnpj_lookup
 
-resp = cnpj_lookup("5", "F99999", "07.150.434/0001-17", "256016631")
+resp = cnpj_lookup(coligada="5", code="F99999", cnpj="07.150.434/0001-17", ie="256016631")
 
 create_new_customer_vendor(
     companyId=resp["companyId"],
