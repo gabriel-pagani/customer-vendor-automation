6b6b8b3e61d4fe0e2d30bac0bf8bde4d877df9da
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-01-21 15:05:43 -0300

    Refactor cnpj_lookup function to include additional parameters and enhance response structure

--- src/apis/receitaws.py
+++ src/apis/receitaws.py
@@ -1,7 +1,31 @@
 import requests
 
 
-def cnpj_lookup(cnpj: str):
+def cnpj_lookup(companyId: str, code: str, type: int, contributor: int, cnpj: str):
     formatted_cnpj = cnpj.replace(".", "").replace("/", "").replace("-", "").strip()
-    response = requests.get(f"https://receitaws.com.br/v1/cnpj/{formatted_cnpj}").json()
+    resp = requests.get(f"https://receitaws.com.br/v1/cnpj/{formatted_cnpj}").json()
+    
+    response = {
+        "companyId": companyId,
+        "code": code,
+        "type": type,
+        "contributor": contributor,
+
+        "shortName": resp["fantasia"],
+        "name": resp["nome"],
+        "mainNIF": resp["cnpj"],
+        "stateRegister": None,
+        "zipCode": resp["cep"].replace(".", "").replace("-", "").strip(),
+        "streetType": None,
+        "streetName": resp["logradouro"],
+        "number": resp["numero"],
+        "districtType": None,
+        "district": resp["bairro"],
+        "countryInternalId": None,
+        "stateCode": resp["uf"],
+        "cityInternalId": None,
+        "phoneNumber": resp["telefone"].replace("(", "").replace(")", "").replace(" ", "").replace("-", "").strip(),
+        "email": resp["email"]
+    }
+    
     return response
