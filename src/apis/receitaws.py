b339917c45d97cacfcd059dce513199683f1f25d
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-01-21 14:33:36 -0300

    Fix formatting of string replacements in cnpj_lookup function

--- src/apis/receitaws.py
+++ src/apis/receitaws.py
@@ -2,6 +2,6 @@ import requests
 
 
 def cnpj_lookup(cnpj: str):
-    formatted_cnpj = cnpj.replace('.', '').replace('/', '').replace('-', '').strip()
+    formatted_cnpj = cnpj.replace(".", "").replace("/", "").replace("-", "").strip()
     response = requests.get(f"https://receitaws.com.br/v1/cnpj/{formatted_cnpj}")
     return response.json()
