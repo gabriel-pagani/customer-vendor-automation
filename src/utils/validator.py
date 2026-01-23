db2058a45ea04122c49f632e96e6f51b7a7d6a74
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-01-23 08:39:27 -0300

    Implement CNPJ validation function in validator.py for improved data integrity

--- src/utils/validator.py
+++ src/utils/validator.py
@@ -1,1 +1,18 @@
-import re
+from itertools import cycle
+
+
+def is_valid_cnpj(cnpj: str) -> bool:
+    if len(cnpj) != 14:
+        return False
+
+    if cnpj in (c * 14 for c in "1234567890"):
+        return False
+
+    cnpj_r = cnpj[::-1]
+    for i in range(2, 0, -1):
+        cnpj_enum = zip(cycle(range(2, 10)), cnpj_r[i:])
+        dv = sum(map(lambda x: int(x[1]) * x[0], cnpj_enum)) * 10 % 11
+        if cnpj_r[i - 1:i] != str(dv % 10):
+            return False
+
+    return True
