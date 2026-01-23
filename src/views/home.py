29c37683ea94835c6645010199cb69adb84843fb
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-01-23 08:24:53 -0300

    Enhance CNPJ management in HomeView: improve list update logic, add error handling for input fields, and refine UI elements for better user experience

--- src/views/home.py
+++ src/views/home.py
@@ -8,14 +8,85 @@ class HomeView:
         self.customers_vendors = dict()
     
     def show(self):
-        def update_list_of_cnpjs(e):
-            ...
+        def update_list_of_cnpjs():
+            list_of_cnpjs.controls.clear()
+            for cnpj, data in self.customers_vendors.items():
+                list_of_cnpjs.controls.append(
+                    ft.Container(
+                        content=ft.Row(
+                            controls=[
+                                ft.Icon(ft.Icons.BUSINESS, color=ft.Colors.GREY_900),
+                                ft.Column(
+                                    controls=[
+                                        ft.Text(f"Cnpj: {cnpj}", weight="bold"),
+                                        ft.Text(f"Coligada: {data['codcoligada']} | Tipo: {'Cliente' if data['type'] == 'c' else 'Fornecedor'}")
+                                    ], 
+                                    expand=True
+                                ),
+                                ft.IconButton(
+                                    icon=ft.Icons.DELETE,
+                                    icon_color=ft.Colors.RED,
+                                    tooltip="Remove cnpj",
+                                    on_click=lambda e, cj=cnpj: remove_cnpj_from_list(cj)
+                                )
+                            ],
+                            alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+                        ),
+                        padding=10,
+                        border=ft.Border.all(1, ft.Colors.GREY_300),
+                        border_radius=5
+                    )
+                )
+
+            self.page.update()
 
         def add_cnpj_to_list(e):
-            ...
+            codcoligada_input.error_text = None
+            cnpj_input.error = None
+            type_input.error_text = None
+            
+            has_error = False
+
+            if not codcoligada_input.value:
+                codcoligada_input.error_text = "Required field!"
+                has_error = True
+
+            if not cnpj_input.value:
+                cnpj_input.error = "Required field!"
+                has_error = True
+
+            if cnpj_input.value.strip() in self.customers_vendors:
+                cnpj_input.error = "This cnpj has already been added!"
+                has_error = True
+            
+            if not type_input.value:
+                type_input.error_text = "Required field!"
+                has_error = True
+
+            if has_error:
+                codcoligada_input.update()
+                cnpj_input.update()
+                type_input.update()
+                return
+
+
+            self.customers_vendors[cnpj_input.value.strip()] = {
+                "codcoligada": codcoligada_input.value,
+                "ie": ie_input.value.strip() if ie_input.value else "",
+                "type": type_input.value
+            }
+            
+            cnpj_input.value = ""
+            ie_input.value = ""
+            
+            update_list_of_cnpjs()
+            show_message(self.page, 1, "Cnpj added successfully!")
 
         def remove_cnpj_from_list(cnpj):
-            ...
+            if cnpj in self.customers_vendors:
+                del self.customers_vendors[cnpj]
+                update_list_of_cnpjs()
+                show_message(self.page, 1, "Cnpj successfully removed!")
 
         def start_automation(e):
             ...
@@ -54,7 +125,7 @@ class HomeView:
 
         add_cnpj_button = ft.IconButton(
             icon=ft.Icons.ADD_CIRCLE,
-            icon_color=ft.Colors.GREY_900,
+            icon_color=ft.Colors.GREEN,
             icon_size=40,
             tooltip="Add cnpj",
             on_click=add_cnpj_to_list,
